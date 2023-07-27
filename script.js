879fbe7207400fd707a379ef8aaedb053d04228e
Dhari2920 <dhari2920@gmail.com>
2023-07-27 10:39:37 +0530

    updated getDate

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //toDateString
-const a = new Date();
-console.log(a.toDateString());
+//const a = new Date();//date object
+//console.log(a.toDateString());Thu jul 27 2023
+//getDate()
+const b = new Date();//date object
+console.log(b.getDate());//27
